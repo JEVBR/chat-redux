a31f51979950124f9cc14920e10be307fd170525
Jeroen Haisma <J_Haisma@hotmail.com>
2019-06-03 22:52:40 -0300

    done

---
+++ src/reducers/channels_reducer.js
@@ -0,0 +1,15 @@
+// reducers/channels_reducer.js
+
+const channelsReducer = (state, action) => {
+  if (state === undefined) {
+    return [];
+  }
+
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default channelsReducer;
+
---
+++ src/reducers/currentUser_reducer.js
@@ -0,0 +1,15 @@
+// reducers/currentUser_reducer.js
+
+const currentUserReducer = (state, action) => {
+  if (state === undefined) {
+    return [];
+  }
+
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default currentUserReducer;
+
